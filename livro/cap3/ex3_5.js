e9f90aa030140ad639622e73d4ca0384144e4f64
Raissa Marques <raissatkm17@gmail.com>
2025-01-23 20:44:58 -0300

    Programa Raiz Quadrada

---
+++ livro/cap3/ex3_5.js
@@ -0,0 +1,21 @@
+function calcularRaizQuadrada(){
+    var inNumero = document.getElementById("inNumero");
+    var outResposta = document.getElementById("outResposta");
+
+    var numero = Number(inNumero.value);
+
+    if(numero == 0  || isNaN(numero)){
+        alert("Informe um numero válido...");
+        inNumero.focus();
+        return;
+    }
+    var raiz = Math.sqrt(numero);
+
+    if(raiz == Math.floor(raiz)){
+        outResposta.textContent = "Raiz: " + raiz;;
+    }else{
+        outResposta.textContent= "Não há raiz exata para " + numero;
+    }
+}
+var btExibir = document.getElementById("btExibir");
+btExibir.addEventListener("click", calcularRaizQuadrada);
