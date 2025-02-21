1b28835912596226291b3728176a493cdbfbcbb7
Raissa Marques <raissatkm17@gmail.com>
2025-02-21 18:34:45 -0300

    .

---
+++ aula7/exer4D.js
@@ -0,0 +1,12 @@
+/*Crie um objeto configuracao que contenha tema: "escuro" e idioma: "pt-BR". Congele esse objeto para evitar que seja modificado.
+configuracao.tema = "claro";
+ // Essa alteração deve ser ignoradaconsole.log(configuracao.tema); // "escuro" */
+const configuracao = Object.freeze({
+    tema: escuro,
+    idioma: pt-BR,
+
+
+
+})
+configuracao.tema = "claro";
+console.log(configuracao.tema); 
